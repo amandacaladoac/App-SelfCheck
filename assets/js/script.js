2ca36e0bc88c1ce529521d83039a4d17bde510aa
vinicius <viniciussantana2003@hotmail.com>
2023-04-07 13:34:36 -0300

    att

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,19 +14,50 @@ function logar() {
 
 }
 
-/*camera check-in*/
-
-function startVideoFromCamera(){
-
-  const specs = {video:{width:395,height: 1000, facingMode: "environment"}}
-
-  navigator.mediaDevices.getUserMedia(specs).then(stream=>{
-
-   const videoElement = document.querySelector("#camera")
-   videoElement.srcObject = stream
-
-  }) .catch(error=>{console.log(error)})
-
+function newpage(){
+   location.href = "login.html";
 }
 
-window.addEventListener("DOMContentLoaded", startVideoFromCamera)
+/*camera check-in*/
+function getUserMedia(constraints) {
+   if (navigator.mediaDevices) {
+     return navigator.mediaDevices.getUserMedia(constraints);
+   }
+     
+   var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+     navigator.mozGetUserMedia || navigator.msGetUserMedia;
+     
+   if (legacyApi) {
+     return new Promise(function (resolve, reject) {
+       legacyApi.bind(navigator)(constraints, resolve, reject);
+     });
+   }
+ }
+ 
+ function getStream (type) {
+   if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+     !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+     alert('User Media API not supported.');
+     return;
+   }
+ 
+   var constraints = { video: {width: 500,height:915,facingMode: "environment" } };
+   
+   getUserMedia(constraints)
+     .then(function (stream) {
+       var mediaControl = document.querySelector(type);
+       
+       if ('srcObject' in mediaControl) {
+         mediaControl.srcObject = stream;
+       } else if (navigator.mozGetUserMedia) {
+         mediaControl.mozSrcObject = stream;
+       } else {
+         mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+       }
+       
+       mediaControl.play();
+     })
+     .catch(function (err) {
+       alert('Error: ' + err);
+     });
+ }
