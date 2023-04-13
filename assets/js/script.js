df2ca976109e8209ec9d0a604507c6305a3d7ed8
Jorge Henrique <113108911+jhsena97@users.noreply.github.com>
2023-04-12 22:08:27 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,23 @@ function getUserMedia(constraints) {
        alert('Error: ' + err);
      });
  }
+
+ document.getElementById("icon-forum").onclick = function () {
+  location.href = "forum.html";
+};
+
+document.getElementById("icon-pontos").onclick = function () {
+  location.href = "pontos-aluno.html";
+};
+
+document.getElementById("icon-checkin").onclick = function () {
+  location.href = "checkin.html";
+};
+
+document.getElementById("icon-perfil").onclick = function () {
+  location.href = "perfil.html";
+};
+
+document.getElementById("icon-sair").onclick = function () {
+  location.href = "index.html";
+};
