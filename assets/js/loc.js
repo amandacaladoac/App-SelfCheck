748a16848041fc556d85901ead66abc2a0bd9170
vinicius <viniciussantana2003@hotmail.com>
2023-04-14 20:31:49 -0300

    att

--- assets/js/loc.js
+++ assets/js/loc.js
@@ -1,27 +1,34 @@
 var map;
-var targetLat = -8.052396398095212;
-var targetLng = -34.88523292829168;
+var targetLat = -8.05229;
+var targetLng = -34.88519;
 var targetRadius = 100; // em metros
+var checkInBtn = document.getElementById('check-in-btn');
 
 function success(pos){
   console.log(pos.coords.latitude, pos.coords.longitude);
 
   if (map === undefined) {
     map = L.map('mapa').setView([pos.coords.latitude, pos.coords.longitude], 22);
+    // Adiciona uma camada do OpenStreetMap ao mapa
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(map);
   } else {
-    map.remove();
-    map = L.map('mapa').setView([pos.coords.latitude, pos.coords.longitude], 22);
+    map.setView([pos.coords.latitude, pos.coords.longitude], 22);
   }
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
 
   L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
     .bindPopup('Eu estou aqui!')
     .openPopup();
 
-}
+  // Calcula a distância entre a localização do usuário e a localização permitida
+  var distance = map.distance([pos.coords.latitude, pos.coords.longitude], [targetLat, targetLng]);
+
+  // Habilita ou desabilita o botão com base na distância
+  checkInBtn.disabled = (distance > targetRadius);
+  checkInBtn.style.backgroundColor = distance > targetRadius ? "#A1A2A6" : "initial";
+};
 
 function error(err){
   console.log(err);
@@ -32,19 +39,15 @@ var watchID = navigator.geolocation.watchPosition(success, error, {
   timeout: 5000
 });
 
-// Calcula a distância entre a localização do usuário e a localização permitida
-var distance = map.distance([pos.coords.latitude, pos.coords.longitude], [targetLat, targetLng]);
-
-// Adiciona o ouvinte de eventos ao botão de check-in e habilita ou desabilita o botão com base na distância
-var checkInBtn = document.getElementById('check-in-btn');
-checkInBtn.disabled = (distance > targetRadius);
-checkInBtn.style.backgroundColor = distance > targetRadius ? "#A1A2A6" : "initial";
-checkInBtn.removeEventListener('click', checkInBtnClick);
 var checkInBtnClick = function() {
+  var pos = map.getCenter();
+  var distance = map.distance([pos.lat, pos.lng], [targetLat, targetLng]);
+
   if (distance <= targetRadius) {
     alert('Check-in realizado com sucesso!');
   } else {
     alert('Você está fora da área permitida!');
   }
 };
+
 checkInBtn.addEventListener('click', checkInBtnClick);
