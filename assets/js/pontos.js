d3438979382d4e65726687197a7042bdc9c42257
Jorge Henrique <113108911+jhsena97@users.noreply.github.com>
2023-05-25 11:48:59 -0300

    att

--- assets/js/pontos.js
+++ assets/js/pontos.js
@@ -37,12 +37,13 @@ function atualizarPontos() {
   updateProgress1(pontos);
   updateProgress2(pontos);
   updateProgress3(pontos);
+  updateProgress4(pontos);
 }
 
 function updateProgress1(points) {
   var progressBar = document.querySelector('.recompensa-pontos1 .progresso-barra div');
   var progressLabel = document.querySelector('.recompensa-pontos1 .progresso-label');
-  var totalPoints = 40;
+  var totalPoints = 90;
   var progressPercent = (points / totalPoints) * 100;
   progressPercent = Math.min(progressPercent, 100);
   progressBar.style.width = progressPercent.toFixed(0) + '%';
@@ -52,7 +53,7 @@ function updateProgress1(points) {
 function updateProgress2(points) {
   var progressBar = document.querySelector('.recompensa-pontos2 .progresso-barra2 div');
   var progressLabel = document.querySelector('.recompensa-pontos2 .progresso-label2');
-  var totalPoints = 60;
+  var totalPoints = 80;
   var progressPercent = (points / totalPoints) * 100;
   progressPercent = Math.min(progressPercent, 100);
   progressBar.style.width = progressPercent.toFixed(0) + '%';
@@ -62,7 +63,17 @@ function updateProgress2(points) {
 function updateProgress3(points) {
   var progressBar = document.querySelector('.recompensa-pontos3 .progresso-barra3 div');
   var progressLabel = document.querySelector('.recompensa-pontos3 .progresso-label3');
-  var totalPoints = 100;
+  var totalPoints = 150;
+  var progressPercent = (points / totalPoints) * 100;
+  progressPercent = Math.min(progressPercent, 100);
+  progressBar.style.width = progressPercent.toFixed(0) + '%';
+  progressLabel.textContent = progressPercent.toFixed(0) + '%';
+}
+
+function updateProgress4(points) {
+  var progressBar = document.querySelector('.recompensa-pontos4 .progresso-barra4 div');
+  var progressLabel = document.querySelector('.recompensa-pontos4 .progresso-label4');
+  var totalPoints = 120;
   var progressPercent = (points / totalPoints) * 100;
   progressPercent = Math.min(progressPercent, 100);
   progressBar.style.width = progressPercent.toFixed(0) + '%';
